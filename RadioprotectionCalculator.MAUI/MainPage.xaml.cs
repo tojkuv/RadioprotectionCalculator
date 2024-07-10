a6a946bdbb8fcc15fe96db41e9113a033a8e66af
Livan <tojkuv@outlook.com>
2024-07-10 00:11:58 -0500

    MAUI Blazor Implementation

---
+++ RadioprotectionCalculator.MAUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RadioprotectionCalculator.MAUI;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ RadioprotectionCalculator.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RadioprotectionCalculator.MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
